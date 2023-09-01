3f4ae6bae64a1a4f4a93e9b9344c1f29f8cc7914
Elisa Rogge <elisa.rogge59@gmail.com>
2023-09-01 13:37:02 +0200

    show recipe card

---
+++ index.js
@@ -0,0 +1,70 @@
+function getRecipeCardDOM(recipe) {
+    const picture = `img/${recipe.image}`;
+    const article = document.createElement('article');
+    const cardInfo = document.createElement('div');
+    article.className = 'recipe-card';
+    cardInfo.className = 'card-info';
+    const img = document.createElement('img');
+    img.setAttribute("src", picture);
+    const h2 = document.createElement('h2');
+    h2.textContent = recipe.name;
+    
+    const recette = document.createElement('div');
+    recette.textContent = 'Recette';
+    recette.style.color = 'grey';
+    recette.style.fontWeight = '600';
+    const ingredientTitle = document.createElement('div');
+    ingredientTitle.textContent = 'Ingrédients';
+    ingredientTitle.style.color = 'grey';
+    ingredientTitle.style.fontWeight = '600';
+    const description = document.createElement('span');
+    description.textContent = recipe.description;
+    description.className = "description";
+    
+    const ingredientList = document.createElement('div');
+    ingredientList.className = 'ingredients'
+    recipe.ingredients.forEach((ingredient) => {
+        const ingredientItem = document.createElement('div');
+        ingredientItem.className = 'ingredient-item'
+        const ingredientQuantity = document.createElement('div');
+        ingredientQuantity.className = 'quantity'
+        let ingredientText = ingredient.ingredient;
+        let ingredientQty = ''
+        
+        if (ingredient.quantity) {
+            ingredientQty = `${ingredient.quantity} `;
+        }
+        
+        if (ingredient.unit) {
+            ingredientQty = `${ingredient.unit}`;
+        }
+        if (ingredient.unit && ingredient.quantity) {
+            ingredientQty = `${ingredient.quantity} ${ingredient.unit}`;
+        }
+        
+        ingredientItem.textContent = ingredientText;
+        ingredientQuantity.textContent = ingredientQty;
+        ingredientList.appendChild(ingredientItem);
+        ingredientItem.appendChild(ingredientQuantity);
+    });
+    
+    article.appendChild(img);
+    article.appendChild(cardInfo);
+    cardInfo.appendChild(h2);
+    cardInfo.appendChild(recette);
+    cardInfo.appendChild(description);
+    cardInfo.appendChild(ingredientTitle);
+    cardInfo.appendChild(ingredientList);
+    
+    return article;
+}
+
+async function displayData(data) {
+    const cardSection = document.querySelector(".card");
+    data.forEach((recipe) => {
+        const userCardDOM = getRecipeCardDOM(recipe);
+        cardSection.appendChild(userCardDOM);
+    });
+}
+
+displayData(recipes);
